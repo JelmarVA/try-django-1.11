e115cbb2de45314092f8cf8e9e742d95c54bf421
jelmar <jelmar.vanaert@gmail.com>
2018-02-15 21:07:33 +0100

    added restaurant function based list view

--- src/restaurants/models.py
+++ src/restaurants/models.py
@@ -7,4 +7,6 @@ class RestaurantLocation(models.Model):
     category        = models.CharField(max_length=120, null=True, blank=True)
     timestamp       = models.DateTimeField(auto_now_add=True)
     updated         = models.DateTimeField(auto_now=True)
-    my_date_field   = models.DateField(auto_now=False, auto_now_add=False)
+    #my_date_field   = models.DateField(auto_now=False, auto_now_add=False)
+    def __str__(self):
+        return self.name
--- src/restaurants/views.py
+++ src/restaurants/views.py
@@ -2,29 +2,18 @@ from django.shortcuts import render
 import random
 from django.views import View
 from django.views.generic import TemplateView
+from .models import RestaurantLocation
 
 # Create your views here.
 # function based view
-class HomeView(TemplateView):
-    template_name = 'home.html'
 
-    def get_context_data(self,*args , **kwargs):
-        context = super(HomeView, self).get_context_data(*args, **kwargs)
-
-        num = None
-        condtion_bool_item = True
-
-        if condtion_bool_item:
-            num = random.randint(0, 200)
-
-        some_list = [random.randint(0, 100000), random.randint(0, 10000), random.randint(0, 100000)]
-
-        context = {
-            "num": num,
-            "some_list": some_list
-        }
-
-        return context
+def restaurant_listview(request):
+    template_name = 'restaurants/restaurants_list.html'
+    querysel = RestaurantLocation.objects.all()
+    context = {
+        "object_list" : querysel
+    }
+    return render(request, template_name, context)
 
 
 
