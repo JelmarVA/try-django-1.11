a97f1d0556a55a3783013d05c1b96eff4f838cf1
jelmar <jelmar.vanaert@gmail.com>
2018-02-15 19:42:16 +0100

    added TemplateView instead of function based views

--- src/restaurants/views.py
+++ src/restaurants/views.py
@@ -1,8 +1,32 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+import random
+from django.views import View
+from django.views.generic import TemplateView
 
 # Create your views here.
 # function based view
-def home(request) :
-    return HttpResponse("hello")
-    #return render(request, "home.html", {})
+class HomeView(TemplateView):
+    template_name = 'home.html'
+
+    def get_context_data(self,*args , **kwargs):
+        context = super(HomeView, self).get_context_data(*args, **kwargs)
+
+        num = None
+        condtion_bool_item = True
+
+        if condtion_bool_item:
+            num = random.randint(0, 200)
+
+        some_list = [random.randint(0, 100000), random.randint(0, 10000), random.randint(0, 100000)]
+
+        context = {
+            "num": num,
+            "some_list": some_list
+        }
+
+        return context
+
+
+
+
+
