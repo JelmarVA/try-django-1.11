0d364b9d4c1b5c5b59e18b22ebf899d4023c33c5
jelmar <jelmar.vanaert@gmail.com>
2018-02-16 15:54:44 +0100

    added a class based ListView

--- src/restaurants/views.py
+++ src/restaurants/views.py
@@ -1,8 +1,10 @@
+from django.db.models import Q
 from django.shortcuts import render
 import random
 from django.views import View
-from django.views.generic import TemplateView
+from django.views.generic import ListView, TemplateView
 from .models import RestaurantLocation
+
 
 # Create your views here.
 # function based view
@@ -15,7 +17,19 @@ def restaurant_listview(request):
     }
     return render(request, template_name, context)
 
+class RestaurantListView(ListView):
+    queryset =  RestaurantLocation.objects.all()
+    template_name = 'restaurants/restaurants_list.html'
 
+    def get_queryset(self):
 
-
+        slug = self.kwargs.get("slug")
+        if slug:
+            queryset = RestaurantLocation.objects.filter(
+                Q(category__iexact=slug) |
+                Q(category__contains=slug)
+            )
+        else:
+            queryset = RestaurantLocation.objects.all()
+        return queryset
 
